ebdd6e41a27b533c7c6decf9931aeb58d947fd78
Raphael Vieira <raphael5254@outlook.com>
2020-11-26 10:44:45 -0300

    Finished layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,24 @@
 const $faqQuestions = document.querySelectorAll('.faq__question');
 const $questionAnswers = document.querySelectorAll('.question__answer');
 const $questionTitles = document.querySelectorAll('.question__title');
+const $questionArrows = document.querySelectorAll('.question__arrow');
 
-$faqQuestions.forEach((elem, index) => {
-    elem.addEventListener('click', (e) => {
+let activeQuestion = null;
+
+$faqQuestions.forEach((question, index) => {
+    question.addEventListener('click', (e) => {
         e.preventDefault();
+        if(activeQuestion !== index && activeQuestion !== null) {
+        $questionTitles[activeQuestion].classList.remove('active');
+        $questionAnswers[activeQuestion].classList.remove('open');
+        $questionArrows[activeQuestion].classList.remove('invert');
+        }
+
         $questionTitles[index].classList.toggle('active');
         $questionAnswers[index].classList.toggle('open');
+        $questionArrows[index].classList.toggle('invert');
+        activeQuestion = index;
+        
     })
 })
+
